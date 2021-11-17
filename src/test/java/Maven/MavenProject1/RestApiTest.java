f1ee5af7d3da001a8adb85b121f35e904b1ff6f8
Ravi <ravihanu1992@gmail.com>
2021-11-17 16:20:11 +0530

    third commit

--- src/test/java/Maven/MavenProject1/RestApiTest.java
+++ src/test/java/Maven/MavenProject1/RestApiTest.java
@@ -10,6 +10,12 @@ public class RestApiTest {
 		 System.out.println("postJira");
 		 System.out.println("post1Jira");
 		 System.out.println("post2Jira");
+		 System.out.println("post3ira");
+		 System.out.println("post4Jira");
+		 System.out.println("post5Jira");
+		 System.out.println("post6ira");
+		 System.out.println("post7Jira");
+		 System.out.println("post8Jira");
 	}
 	
 	@Test
